c710c3263d1d76363c489cc58b2017d580b82cc7
27ugeen <27ugeen@gmail.com>
2022-09-05 11:32:44 +0300

    Initial Commit

---
+++ testRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testRxSwift
+//
+//  Created by GiN Eugene on 5/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
